cc6726960d131bc18cfd97ed32f6fe9b6ecb6517
josephriosio <blankworldwide@gmail.com>
2019-04-26 09:18:45 -0700

    added get endpoint to project

--- routes/project.js
+++ routes/project.js
@@ -4,4 +4,19 @@ const projectDb = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
+//get endpoint
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await projectDb.get(id);
+    if (project.length === 0) {
+      res.status(400).json({ error: "id doesnt exist" });
+    } else {
+      res.json(project);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
